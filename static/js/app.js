a86f23bd3b542905f5d7705a04a669bcb59a2c00
bigsinoos <pcfeeler@gmail.com>
2015-01-03 21:04:46 +0330

    Fix lat lng

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,3 @@
-/*------------------------------------------------------*\
-
-     _ _ _ _____ _____ _____ _____ _____ _____ __      _____ _____ _____ 
-    | | | |   __|   __|     |  |  |     |     |  |    |     | __  |   __|
-    | | | |__   |__   |   --|     |  |  |  |  |  |__ _|  |  |    -|  |  |
-    |_____|_____|_____|_____|__|__|_____|_____|_____|_|_____|__|__|_____|
-
-    (C) 2014 - Sallar Kaboli <sallar.kaboli@gmail.com>
-    Web Standards School <wsschool.org>.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-\*------------------------------------------------------*/
-
 var ie = (function(){
 
     var undef,
@@ -66,7 +40,7 @@ if( $('.map-container').length > 0 )
 
     mapOptions = {
         zoom               : 8,
-        center             : new google.maps.LatLng(35.732766,51.389306),
+        center             : new google.maps.LatLng(0,0),
         mapTypeId          : google.maps.MapTypeId.ROADMAP,
         zoom               : 16,
         disableDefaultUI   : true,
